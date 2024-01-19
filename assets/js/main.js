14fc3f973161820f2e43f4618b7641fdaa3808de
MatteoSala97 <matteo111997@gmail.com>
2024-01-19 14:16:05 +0100

    fix: milestone 0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,41 @@
 const member1 = {
-    firstName : "Wayne"
-    lastName : "Barnett"
-    role : "Founder & CEO"
+    firstName : "Wayne",
+    lastName : "Barnett",
+    role : "Founder & CEO",
     portrait : "./assets/img/wayne-barnett-founder-ceo.jpg"
 }
 const member2 = {
-    firstName : "Angela"
-    lastName : "Carrol"
-    role : "Chief Editor"
+    firstName : "Angela",
+    lastName : "Carrol",
+    role : "Chief Editor",
     portrait : "./assets/img/angela-caroll-chief-editor.jpg"
 }
 const member3 = {
-    firstName : "Walter"
-    lastName : "Gordon"
-    role : "Office Manager"
+    firstName : "Walter",
+    lastName : "Gordon",
+    role : "Office Manager",
     portrait : "./assets/img/walter-gordon-office-manager.jpg"
 }
 const member4 = {
-    firstName : "Angela"
-    lastName : "Lopez"
-    role : "Social Media Manager"
+    firstName : "Angela",
+    lastName : "Lopez",
+    role : "Social Media Manager",
     portrait : "./assets/img/angela-lopez-social-media-manager.jpg"
 }
 const member5 = {
-    firstName : "Scott"
-    lastName : "Estrada"
-    role : "Developer"
+    firstName : "Scott",
+    lastName : "Estrada",
+    role : "Developer",
     portrait : "./assets/img/scott-estrada-developer.jpg"
 }
 const member6 = {
-    firstName : "Barbara"
-    lastName : "Ramos"
-    role : "Graphic Designer"
+    firstName : "Barbara",
+    lastName : "Ramos",
+    role : "Graphic Designer",
     portrait : './assets/img/barbara-ramos-graphic-designer.jpg'
 }
+
+let arrayMembers = [member1, member2, member3, member4,member5, member6]
+
+console.log(arrayMembers)
+
