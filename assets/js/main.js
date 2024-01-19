8a72bcae06fdc66747c0f5cabc5d4b26f399b5d9
MatteoSala97 <matteo111997@gmail.com>
2024-01-19 14:11:29 +0100

    feat: config + milestone 0

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const member1 = {
+    firstName : "Wayne"
+    lastName : "Barnett"
+    role : "Founder & CEO"
+    portrait : "./assets/img/wayne-barnett-founder-ceo.jpg"
+}
+const member2 = {
+    firstName : "Angela"
+    lastName : "Carrol"
+    role : "Chief Editor"
+    portrait : "./assets/img/angela-caroll-chief-editor.jpg"
+}
+const member3 = {
+    firstName : "Walter"
+    lastName : "Gordon"
+    role : "Office Manager"
+    portrait : "./assets/img/walter-gordon-office-manager.jpg"
+}
+const member4 = {
+    firstName : "Angela"
+    lastName : "Lopez"
+    role : "Social Media Manager"
+    portrait : "./assets/img/angela-lopez-social-media-manager.jpg"
+}
+const member5 = {
+    firstName : "Scott"
+    lastName : "Estrada"
+    role : "Developer"
+    portrait : "./assets/img/scott-estrada-developer.jpg"
+}
+const member6 = {
+    firstName : "Barbara"
+    lastName : "Ramos"
+    role : "Graphic Designer"
+    portrait : './assets/img/barbara-ramos-graphic-designer.jpg'
+}
